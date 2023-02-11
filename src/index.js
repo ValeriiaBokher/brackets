3d6e2d241d1c4751d1c01de557a030c78cc060aa
Valeriia Bokher <valeriiabokher@gmaol.com>
2023-02-11 21:53:29 +0200

    docs: create

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	var string = '';
+	for (let i = 0; i < str.length; i++) {
+		for (j = 0; j < bracketsConfig.length; j++) {
+			if (
+				bracketsConfig[j].indexOf(str[i]) == 1 &&
+				string[string.length - 1] == str[i]
+			) {
+				string = string.slice(0, -1);
+			} else if (
+				bracketsConfig[j].indexOf(str[i]) == 0 &&
+				string[string.length - 1] == str[i]
+			) {
+				string = string.slice(0, -1);
+			} else if (
+				bracketsConfig[j].indexOf(str[i]) == 0 ||
+				bracketsConfig[j].indexOf(str[i]) == 1
+			) {
+				string += bracketsConfig[j][1];
+			}
+		}
+	}
+	if (string.length == 0) {
+		return true;
+	} else {
+		return false;
+	}
+};
